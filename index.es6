d363bd9006e9ed1dd6b357d8ef830c443f8e8379
Jason Quense <monastic.panic@gmail.com>
2017-01-17 13:26:54 -0500

    [fixed] remove meta from props

---
+++ index.es6
@@ -0,0 +1,37 @@
+let React = require('react')
+  , widgets = require('react-widgets');
+
+function createInput(Component, defaults){
+  class FormInput extends React.Component {
+    static defaultProps = defaults || {}
+    render() {
+      const { meta, ...props } = this.props;
+      return <Component {...props} />
+    }
+  }
+}
+
+let types = Object.create(null);
+
+types.combobox = createInput(widgets.Combobox)
+types.dropdownlist = createInput(widgets.DropdownList)
+types.calendar = createInput(widgets.Calendar)
+types.selectlist = createInput(widgets.SelectList)
+
+types.date =
+  types.datepicker = createInput(widgets.DateTimePicker, { time: false })
+
+types.time =
+  types.timepicker = createInput(widgets.DateTimePicker, { calendar: false, date: false })
+
+types.datetime =
+  types['datetime-local'] =
+     types.datetimepicker = createInput(widgets.DateTimePicker)
+
+types.number =
+  types.numberpicker = createInput(widgets.NumberPicker)
+
+types.array =
+  types.multiselect = createInput(widgets.Multiselect)
+
+module.exports = types
--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-var React = require('react')
-  , widgets = require('react-widgets');
+'use strict';
 
-function wrapWithDefaults(Component, defaults){
-  return React.createClass({
-    getDefaultProps: function(){ return defaults },
-    render: function(){
-      return React.createElement(Component, this.props)
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var React = require('react'),
+    widgets = require('react-widgets');
+
+function createInput(Component, defaults) {
+  var FormInput = function (_React$Component) {
+    _inherits(FormInput, _React$Component);
+
+    function FormInput() {
+      _classCallCheck(this, FormInput);
+
+      return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
     }
-  })
+
+    FormInput.prototype.render = function render() {
+      var _props = this.props,
+          meta = _props.meta,
+          props = _objectWithoutProperties(_props, ['meta']);
+
+      return React.createElement(Component, props);
+    };
+
+    return FormInput;
+  }(React.Component);
+
+  FormInput.defaultProps = defaults || {};
 }
 
 var types = Object.create(null);
 
+types.combobox = createInput(widgets.Combobox);
+types.dropdownlist = createInput(widgets.DropdownList);
+types.calendar = createInput(widgets.Calendar);
+types.selectlist = createInput(widgets.SelectList);
 
-types.combobox       = widgets.Combobox
-types.dropdownlist   = widgets.DropdownList
-types.calendar       = widgets.Calendar
-types.selectlist     = widgets.SelectList
+types.date = types.datepicker = createInput(widgets.DateTimePicker, { time: false });
 
-types.date           =
-  types.datepicker   = wrapWithDefaults(widgets.DateTimePicker, { time: false })
+types.time = types.timepicker = createInput(widgets.DateTimePicker, { calendar: false, date: false });
 
-types.time           =
-  types.timepicker   = wrapWithDefaults(widgets.DateTimePicker, { calendar: false })
+types.datetime = types['datetime-local'] = types.datetimepicker = createInput(widgets.DateTimePicker);
 
-types.datetime =
-types['datetime-local'] = 
-  types.datetimepicker  = widgets.DateTimePicker
+types.number = types.numberpicker = createInput(widgets.NumberPicker);
 
-types.number         = 
-  types.numberpicker = widgets.NumberPicker
+types.array = types.multiselect = createInput(widgets.Multiselect);
 
-types.array          =
-  types.multiselect  = widgets.Multiselect
-
-module.exports = types
+module.exports = types;
